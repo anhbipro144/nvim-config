b4a4edf7048f2654e6eeb146f9f330de2eb431a3
Lanh (Neo) Nguyen <lanh.nguyen.tpv@one-line.com>
2025-09-23 22:28:54 +0700

    add yarn & mini config for ksb, disable AI autosugesstion

--- lua/kickstart/plugins/copilot.lua
+++ lua/kickstart/plugins/copilot.lua
@@ -2,5 +2,7 @@ return {
   "github/copilot.vim",
   config = function()
     vim.cmd("Copilot enable")
+    vim.g.copilot_filetypes = { ['*'] = false }
+    vim.keymap.set('i', '<S-x>', 'copilot#Suggest()', { silent = true, expr = true, noremap = true })
   end,
 }
