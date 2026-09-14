f6ac764b07b9d3cc977ab57ec01104deee7350bf
Lanh (Neo) Nguyen <lanh.nguyen.tpv@one-line.com>
2026-09-14 08:56:28 +0700

    update tree-sitter & ts-text-object

--- lua/kickstart/plugins/treecontext.lua
+++ lua/kickstart/plugins/treecontext.lua
@@ -1,67 +1,44 @@
 return {
-  "nvim-treesitter/nvim-treesitter-textobjects",
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  branch = 'main',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
-    require 'nvim-treesitter.configs'.setup {
-      textobjects = {
-        select = {
-          enable = true,
+    local select = require 'nvim-treesitter-textobjects.select'
 
-          -- Automatically jump forward to textobj, similar to targets.vim
-          lookahead = true,
-
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-
-            ["aC"] = "@class.outer",
-            ["iC"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-
-
-            ["am"] = "@call.outer", -- m = method/call
-            ["im"] = "@call.inner",
-
-
-            ["ac"] = "@conditional.outer",
-            ["ic"] = "@conditional.inner",
-
-
-            ["al"] = "@loop.outer",
-            ["il"] = "@loop.inner",
-
-            ["ad"] = "@comment.outer",
-            ["id"] = "@comment.inner",
-
-            ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-
-
-            ["a="] = "@assignment.outer",
-            ["i="] = "@assignment.inner",
-          },
-          -- You can choose the select mode (default is charwise 'v')
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * method: eg 'v' or 'o'
-          -- and should return the mode ('v', 'V', or '<c-v>') or a table
-          -- mapping query_strings to modes.
-          selection_modes = {
-            ['@parameter.outer'] = 'v', -- charwise
-            ['@function.outer'] = 'V',  -- linewise
-            ['@class.outer'] = '<c-v>', -- blockwise
-          },
-          -- If you set this to `true` (default is `false`) then any textobject is
-          -- extended to include preceding or succeeding whitespace. Succeeding
-          -- whitespace has priority in order to act similarly to eg the built-in
-          -- `ap`.
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * selection_mode: eg 'v'
-          -- and should return true or false
-          include_surrounding_whitespace = true,
+    require('nvim-treesitter-textobjects').setup {
+      select = {
+        lookahead = true,
+        selection_modes = {
+          ['@parameter.outer'] = 'v',
+          ['@function.outer'] = 'V',
+          ['@class.outer'] = '<c-v>',
         },
+        include_surrounding_whitespace = true,
       },
     }
-  end
+
+    local keymaps = {
+      af = { '@function.outer' },
+      ['if'] = { '@function.inner' },
+      aC = { '@class.outer' },
+      iC = { '@class.inner', nil, 'Select inner part of a class region' },
+      am = { '@call.outer' },
+      im = { '@call.inner' },
+      ac = { '@conditional.outer' },
+      ic = { '@conditional.inner' },
+      al = { '@loop.outer' },
+      il = { '@loop.inner' },
+      ad = { '@comment.outer' },
+      id = { '@comment.inner' },
+      ['as'] = { '@local.scope', 'locals', 'Select language scope' },
+      ['a='] = { '@assignment.outer' },
+      ['i='] = { '@assignment.inner' },
+    }
+
+    for keys, textobject in pairs(keymaps) do
+      vim.keymap.set({ 'x', 'o' }, keys, function()
+        select.select_textobject(textobject[1], textobject[2] or 'textobjects')
+      end, { desc = textobject[3] })
+    end
+  end,
 }
--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -1,32 +1,23 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
     config = function()
-      require 'nvim-treesitter.configs'.setup {
+      local treesitter = require 'nvim-treesitter'
 
-        ensure_installed = { "lua", "vim", "vimdoc", "markdown", "markdown_inline", "typescript", "javascript", 'cpp' },
-
-
-        sync_install = false,
-
-        highlight = {
-          enable = true,
-
-          -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-          -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-          -- the name of the parser)
-          -- list of language that will be disabled
-          -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
+      treesitter.setup {
+        install_dir = vim.fn.stdpath('data') .. '/site',
       }
+      treesitter.install { 'lua', 'vim', 'vimdoc', 'markdown', 'markdown_inline', 'python', 'typescript', 'javascript', 'cpp' }
+
+      local group = vim.api.nvim_create_augroup('kickstart-treesitter', { clear = true })
+      vim.api.nvim_create_autocmd('FileType', {
+        group = group,
+        callback = function(event)
+          pcall(vim.treesitter.start, event.buf)
+        end,
+      })
     end
   },
 }
