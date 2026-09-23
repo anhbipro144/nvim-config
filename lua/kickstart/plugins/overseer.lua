e5284f74f79160021b0b34c342b49cef7dd20405
Lanh (Neo) Nguyen <lanh.nguyen.tpv@one-line.com>
2026-09-23 22:04:06 +0700

    add overseer templates

--- lua/kickstart/plugins/overseer.lua
+++ lua/kickstart/plugins/overseer.lua
@@ -39,6 +39,41 @@ return {
     }
 
     overseer.register_template {
+      name = 'BFF',
+      desc = 'Run the NPRD BFF from the active Git worktree',
+      builder = function()
+        local worktree_root = vim.fn.getcwd()
+
+        return {
+          name = string.format('BFF [%s]', vim.fn.fnamemodify(worktree_root, ':t')),
+          cmd = { '/home/neo/.nix-profile/bin/mise', 'exec', '--', 'yarn', 'start:local' },
+          cwd = vim.fs.joinpath(worktree_root, 'bff'),
+          components = { 'default' },
+        }
+      end,
+    }
+
+    overseer.register_template {
+      name = 'Backend',
+      desc = 'Run the NPRD gRPC backend from the active Git worktree',
+      builder = function()
+        local worktree_root = vim.fn.getcwd()
+        local backend_dir = vim.fs.joinpath(worktree_root, 'backend')
+
+        return {
+          name = string.format('Backend [%s]', vim.fn.fnamemodify(worktree_root, ':t')),
+          cmd = { vim.fs.joinpath(backend_dir, '.venv', 'bin', 'python'), 'main.py', '3011' },
+          cwd = backend_dir,
+          env = {
+            PWD = backend_dir,
+            RUN_ENVIRONMENT = 'local',
+          },
+          components = { 'default' },
+        }
+      end,
+    }
+
+    overseer.register_template {
       name = 'Cloud SQL Proxy (dev)',
       desc = 'Run the development Cloud SQL Proxy on port 5435',
       builder = function()
